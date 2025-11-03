a0b32bd178a7596aee669c489626ade0c1337402
Shiraz Yoel <shiraz.jaul@post.runi.ac.il>
2025-11-03 17:22:23 +0200

    test

--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,12 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
+		double currentValue = double.parseDouble(args(0)) 
+		double rate = double.parseDouble(args(1))  
+		double yearsN = double.parseint(args(2)) 
+		args(2) + 1 =args(3)
+
+		int futureValuex = args(1) * (Math.pow(args(3),args(2)))
+
 	}
 }
